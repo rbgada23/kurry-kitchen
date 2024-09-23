e017f599aaacf030cff37c7fabc825f10d83bbf2
rbgada23 <rishabhgada@gmail.com>
2024-09-23 13:17:10 -0400

    Configured tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import food from './assets/food1.jpg'
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+       <img className="mx-auto" src={food}></img>
+      <h1 className="font-bold text-xl text-center"><i>KurryKitchen</i> under construction !!</h1>
     </div>
   );
 }
